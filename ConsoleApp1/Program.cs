5a9c53bd3d19e4981c06f9f06ac0eb934a369905
Matthew Coleman <K31073@eltham-college.org.uk>
2019-11-20 11:57:52 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int o = 0; o < 12)
+            {
+Console.Beep();
+            }
+            
+        }
+    }
+}
